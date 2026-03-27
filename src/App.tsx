a0f5ee0b08bb5c5362804be5f7f1de29dc1bbdac
Hoon Oh <hc57@naver.com>
2026-03-27 12:23:47 +0900

    Cleanup: App named export

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+// src/App.tsx
 import React from 'react';
-import EditorLayout from '@/components/Layout/EditorLayout';
+import { EditorLayout } from '@/components/Layout/EditorLayout';
 
-export default function App() {
+export function App(): React.ReactElement {
   return <EditorLayout />;
 }
+
+export default App;
